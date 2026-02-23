5a36b3c2b4041169ad09622a4a635355a516055d
ys6999337-droid <ys6999337@gmail.com>
2026-02-23 14:45:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -690,8 +690,10 @@ if ticker:
         if not data.empty:
             # Aapki class ko call kar rahe hain indicators ke liye
                         # TA-Lib ki jagah pandas_ta use kar rahe hain
-            import pandas_ta as ta_lib
-            data.ta.strategy("Common") # Ye RSI, MACD, SMA sab khud nikal lega
+                        # Purani lines ko hata kar ye 2 sahi lines likhein:
+            import pandas_ta as ta
+            data.ta.strategy("All") # 'All' use karein taaki saare indicators aa jayein
+
             df_with_indicators = data
             
             # 3. Graph Dikhana
